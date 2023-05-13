7818a92627e95835659dc7b2ae7d8f377fa5119f
tceriksen <81637530+tceriksen@users.noreply.github.com>
2023-05-13 13:56:44 +0200

    A simpel script that determines a leap year.

---
+++ leapyear.js
@@ -0,0 +1,19 @@
+function isLeap(year) {   
+    
+    if ( year % 4 == 0 ) {
+        if ( year % 100 == 0 ) {
+            if ( year % 400 == 0 ) {
+                return " Leap year.";
+            } else {
+                return "Not leap year.";
+            }
+        } else {
+        return "Leap year.";
+        }
+    } else {
+        return "Not leap year.";
+    }
+}
+
+isLeap(2024); // "Leap year."
+isLeap(2023); // "Not leap year."
